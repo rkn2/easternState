71c6a0f6e2e83e7872fcbb4652fc748cea1f3981
Rebecca Napolitano <rkn2@princeton.edu>
2020-02-20 21:12:47 -0500

    fa works and plotting for vis analysis

--- loadAndRead.py
+++ loadAndRead.py
@@ -29,19 +29,21 @@ def get_spatial_correlation(x, y, W):
     Rc = np.matmul(np.matmul(x_norm.transpose(), W), y_norm)
     return Rc
 
-def makeScree():
+
+def makeScree(fa):
     ev, v = fa.get_eigenvalues()
-    plt.scatter(range(1, df.shape[1] + 1), ev)
-    plt.plot(range(1, df.shape[1] + 1), ev)
+    plt.scatter(range(1, df.shape[1] + 1), np.log(ev))
+    plt.plot(range(1, df.shape[1] + 1), np.log(ev))
     plt.title('Scree Plot')
     plt.xlabel('Factors')
     plt.ylabel('Eigenvalue')
     plt.grid()
     plt.show()
 
+
 # read the cad file
 os.chdir(r"/Volumes/GoogleDrive/My Drive/Documents/Research/easternStatePenitentiary/2020_1_28_files/allOfIt")
-#cadFile = r"2020_01_24_DRAFT_West_Wall_mkr.dxf"
+# cadFile = r"2020_01_24_DRAFT_West_Wall_mkr.dxf"
 cadFile = r"2020-02-06 - DRAFT South Wall.dxf"
 doc = ezdxf.readfile(cadFile)
 
@@ -102,28 +104,58 @@ for i, point in tqdm(enumerate(pointList), total=len(pointList)):
 # newLayers = layers
 # nDistMat = distMat
 
-#list layers i want
-newLayers = ['C-SPALL', 'C-JOIN','W-MRTR', 'C-REPR', 'C-STON', 'E-METL', 'E-VEGT', 'W-STON', 'W-SURF']
-nDistMat = np.ones([num_samples,len(newLayers)])*np.inf
+# # coarse combine
+# newLayers = ['C-SPALL', 'C-JOIN','W-MRTR', 'C-REPR', 'C-STON', 'E-METL', 'E-VEGT', 'W-STON', 'W-SURF']
+# nDistMat = np.ones([num_samples,len(newLayers)])*np.inf
+#
+# for i,entry in enumerate(newLayers):
+#     these_layers = []
+#     for j,layer in enumerate(layers):
+#         if entry in layer:
+#             these_layers.append(j)
+#     if len(these_layers) > 0:
+#         nDistMat[:,i] = np.min(distMat[:,these_layers], axis=1)
+#
+# truthMat = np.exp(-nDistMat**2/1e5)
 
-for i,entry in enumerate(newLayers):
+# specific combine
+newLayers = ['W-MRTR-BCKP', 'W-MRTR-FNSH', 'C-REPR', 'E-METL', 'E-VEGT', 'W-STON-BULG', 'W-STON-RESET']
+
+for layer in layers:
+    for new in newLayers:
+        if layer not in newLayers and bool([ele for ele in newLayers if (ele in layer)]) == False:
+            newLayers.append(layer)
+
+newLayers.append('E-METL-T1')
+newLayers.append('E-METL-T4')
+
+nDistMat = np.ones([num_samples, len(newLayers)]) * np.inf
+for i, entry in enumerate(newLayers):
     these_layers = []
-    for j,layer in enumerate(layers):
+    for j, layer in enumerate(layers):
         if entry in layer:
-            these_layers.append(j)
+            if 'E-METL' in layer:
+                if layer[-1] == '2' or '4':
+                    these_layers.append(j)
+            else:
+                these_layers.append(j)
     if len(these_layers) > 0:
-        nDistMat[:,i] = np.min(distMat[:,these_layers], axis=1)
+        nDistMat[:, i] = np.min(distMat[:, these_layers], axis=1)
+    # print('the layers are: ' + str(these_layers))
 
-truthMat = np.exp(-nDistMat**2/1e5)
+truthMat = np.exp(-nDistMat ** 2 / 8e4)
 
-plt.scatter(xRand, yRand, s=1, c=truthMat[:, 2], vmin=0, vmax=1)
+# to get index of certain layer
+# indices = [i for i, s in enumerate(layers) if 'E-METL-T1' in s]
+
+plt.scatter(xRand, yRand, s=1, c=truthMat[:, 4], vmin=0, vmax=1)
 plt.show()
 
 # regular fa
 fa = FactorAnalyzer()
-numFactors = 5
-#df = pd.DataFrame(truthMat, columns=layers)
-#unnecessaryColumns = [layers[x] for x in [0, 1, 2, 3, 9]]
+numFactors = 6
+# df = pd.DataFrame(truthMat, columns=layers)
+# unnecessaryColumns = [layers[x] for x in [0, 1, 2, 3, 9]]
 df = pd.DataFrame(truthMat, columns=newLayers)
 unnecessaryColumns = [x for i, x in enumerate(newLayers) if np.max(nDistMat[:, i]) == np.Inf]
 df.drop(unnecessaryColumns, axis=1, inplace=True)
@@ -131,9 +163,16 @@ df.dropna(inplace=True)
 fa.analyze(df, numFactors, rotation=None)
 L = np.array(fa.loadings)
 headings = list(fa.loadings.transpose().keys())
-factor_threshold = 0.3
+factor_threshold = 0.5
 for i, factor in enumerate(L.transpose()):
     descending = np.argsort(np.abs(factor))[::-1]
     contributions = [(np.round(factor[x], 2), headings[x]) for x in descending if np.abs(factor[x]) > factor_threshold]
     print('Factor %d:' % (i + 1), contributions)
 
+factor_list = ['W-MRTR-OPEN', 'W-SURF-STAIN-T1']
+for entry in factor_list:
+    index = [i for i, s in enumerate(newLayers) if entry in s]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xRand, yRand, s=1, c=truthMat[:, index[0]], vmin=0, vmax=1)
+    ax.set_title(entry)
